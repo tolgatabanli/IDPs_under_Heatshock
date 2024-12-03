c321883f1ac080099bbac2752868dd7a2ff28d4e
tolgatabanli <tolgatabanli@gmail.com>
2024-12-03 20:34:37 +0100

    tried some transformations, not promising so far

--- aiupred/percentage distribution.py
+++ aiupred/percentage distribution.py
@@ -1,5 +1,6 @@
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open('aiupred_scores.p', 'rb') as fp:
     proteins = pickle.load(fp)
@@ -9,5 +10,14 @@ percentages = dict()
 for name, disorder_array in proteins.items():
     percentages[name] = (disorder_array > 0.5).sum() / len(disorder_array)
 
-plt.hist(percentages.values(), bins=25)
+# Logit Transformation
+# arr = [np.atleast_1d(perc) for perc in percentages.values()]
+# arr = np.concatenate(arr)
+# clipped = np.clip(arr, a_min=1e-6, a_max=1 - 1e-6)
+# logit_values = np.log(clipped / (1 - clipped))
+
+arr = [np.atleast_1d(perc) for perc in percentages.values()]
+arr = np.concatenate(arr)
+plt.hist(arr, bins=50)
+plt.show()
 plt.savefig("hist (bins_50) of percentage of proteins of residues alpha_0.5.png")
