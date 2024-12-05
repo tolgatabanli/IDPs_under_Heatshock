5c7abf641b6fb8c398556d179a745166801abd3a
tolgatabanli <tolgatabanli@gmail.com>
2024-12-05 17:45:38 +0100

    added csv creation

--- aiupred/mean score distribution.py
+++ aiupred/mean score distribution.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+import csv
 
 with open('aiupred_scores.p', 'rb') as fp:
     proteins = pickle.load(fp)
@@ -30,6 +31,11 @@ for prot, scores in proteins.items():
 plt.hist(proteins.values(), bins=50)
 plt.xlabel("Disorder scores")
 plt.title("Distribution of binned(10) mode disorder scores from AIUPred")
-#plt.show()
-plt.savefig("hist (bins_50) of binned(10) mode disorder scores.png")
+plt.show()
+#plt.savefig("hist (bins_50) of binned(10) mode disorder scores.png")
 
+# save the dictionary in a tsv (protein_name, mode)
+csv = "\n".join([prot + '\t' + str(mode) for prot, mode in proteins.items()])
+with open("aiupred_modes_of_proteins.tsv", 'w') as output:
+    output.write("name_uniprot\tscore_mode\n")
+    output.write(csv)
