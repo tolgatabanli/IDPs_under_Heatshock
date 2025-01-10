8694d0e5b5dbdc240aaa7ea3850f13f7c7ef7169
Tolga <tolgatabanli@gmail.com>
2025-01-10 16:32:11 +0100

    corrections

--- real idp comparisons.R
+++ real idp comparisons.R
@@ -1,17 +1,19 @@
 library(readxl)
 library(tidyverse)
 
+# Prepare databases
 biomart_mapping <- read_tsv("IDP decisions/biomart_mapping.tsv")
 alphafold_mode <- read_tsv("IDP decisions/all_proteins_from_alphafold_with_mode.tsv")
 aiupred_mode <- read_tsv("IDP decisions/all_proteins_from_aiupred_with_mode.tsv")
+
 
 d2p2_idps <- readxl::read_xlsx("d2p2_experimental_idps.xlsx",
                           range = "A1:A38",
                           col_names = F)
 colnames(d2p2_idps) <- "ensembl_gene_id"
 d2p2_idps <- d2p2_idps %>%
-  left_join(alphafold_mode, by = "ensembl_gene_id") %>%
-  left_join(aiupred_mode, by = "ensembl_gene_id")
+  left_join(alphafold_mode) %>%
+  left_join(aiupred_mode)
 
 disprot_idps <- read_tsv("DisProt/DisProt release_2024_12.tsv") %>%
   select(acc, disorder_content) %>%
@@ -19,8 +21,10 @@ disprot_idps <- read_tsv("DisProt/DisProt release_2024_12.tsv") %>%
   inner_join(biomart_mapping, by = join_by(acc == uniprotswissprot)) %>%
   select(ensembl_gene_id, disorder_content) %>%
   arrange(disorder_content) %>%
-  left_join(alphafold_mode, by = "ensembl_gene_id") %>%
-  left_join(aiupred_mode, by = "ensembl_gene_id")
+  left_join(alphafold_mode) %>%
+  left_join(aiupred_mode)
+
+# Analyse parameters of real IDPs
 
 # d2p2 AF Mode
 d2p2_idps %>%
