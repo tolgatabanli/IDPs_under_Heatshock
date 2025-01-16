fd9d3b31cf96d6367073be647d2dd150b384e758
Tolga <tolgatabanli@gmail.com>
2025-01-16 17:51:55 +0100

    added all genes' heatmap

--- heatmap.R
+++ heatmap.R
@@ -43,9 +43,26 @@ deseq <- readRDS("deseq_results.Rds") %>%
   purrr::reduce(function(x, y) {
     full_join(x, y)
   }) %>%
-  drop_na() %>% # 498 elements become 491 elements
+  drop_na() %>%
   column_to_rownames() %>%
   as.matrix()
 
 pheatmap(deseq, show_rownames = F,
          main = "Heatmap from DisProt")
+
+# Heatmap with All Proteins
+df_names <- names(deseq)
+deseq <- readRDS("deseq_results.Rds") %>%
+  map(data.frame) %>%
+  map(rownames_to_column) %>%
+  map(select, rowname, log2FoldChange) %>%
+  imap(~ setNames(.x, gsub("log2FoldChange", .y, names(.x)))) %>%
+  purrr::reduce(function(x, y) {
+    full_join(x, y)
+  }) %>%
+  drop_na() %>%
+  column_to_rownames() %>%
+  as.matrix()
+
+pheatmap(deseq, show_rownames = F,
+         main = "Heatmap with All Genes")
