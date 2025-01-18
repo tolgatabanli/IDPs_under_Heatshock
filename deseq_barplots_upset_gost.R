75056cec3cb42be23be27429b95813bcc4538266
Tolga <tolgatabanli@gmail.com>
2025-01-18 12:36:51 +0100

    updated upset plots added diff comparisons for gost at the end

--- deseq_barplots_upset_gost.R
+++ deseq_barplots_upset_gost.R
@@ -73,14 +73,14 @@ deseq_sig_idps <- deseq_results %>%
   
 
 dev.off()
-png("significant plots/upset(ordered_degree)_from_deseq_on_commons_modes.png", width = 1920, height = 1080, res = 150)
+png("significant plots/upset(ordered_freq)_from_deseq_on_commons_modes.png", width = 1920, height = 1080, res = 150)
 upset(fromList(deseq_sig_idps), sets = names(deseq_sig_idps),
       keep.order = T,
       nsets = length(deseq_sig_idps),
       point.size = 3, line.size = 1,
       mainbar.y.label = "Significant IDP Intersection",
       sets.x.label = "Number of IDPs ",
-      order.by = c("degree"),
+      order.by = c("freq"),
       text.scale = c(1.7,1,1.3,1,1,1.3))
 dev.off()
 
@@ -88,6 +88,7 @@ dev.off()
 
 set_base_url("https://biit.cs.ut.ee/gprofiler_beta") # for when the server's down
 
+# Against whole genome
 gost_res <- deseq_sig_idps %>%
   janitor::clean_names() %>%
   map(~ gost(query = .x,
@@ -108,7 +109,7 @@ gost_res %>%
 # GostPlots
 terms <- gost_res %>%
   map(~ `$`(.x, "result")) %>%
-  map(pull, term_id) %>%
+  map(dplyr::pull, term_id) %>%
   purrr::reduce(base::union)
 
 gost_res %>%
@@ -122,3 +123,34 @@ gost_res %>%
   iwalk(~ publish_gostplot(p = .x,
                            filename = paste0("gostplots/", .y, ".png"),
                            highlight_terms = terms))
+
+# Against IDPs: no result
+gost_res <- deseq_sig_idps %>%
+  janitor::clean_names() %>%
+  map(~ gost(query = .x,
+             organism = "scerevisiae",
+             custom_bg = idps,
+             correction_method = "bonferroni"))
+
+gost_res %>%
+  compact() %>%
+  map(~ gostplot(.x, interactive = F)) %>%
+  iwalk(~ publish_gostplot(p = .x,
+                           filename = paste0("gostplots_idp_bg/", .y, ".png")))
+
+# Wildtype 42_30 minus Double_42_30
+wt_heatshock <- base::setdiff(deseq_sig_idps[["Wildtype_42_10"]],
+                              deseq_sig_idps[["Wildtype_37_10"]])
+exp_heatshock <- base::setdiff(deseq_sig_idps[["MSN24.KO_42_10"]],
+                               deseq_sig_idps[["MSN24.KO_37_10"]])
+diff <- base::setdiff(wt_heatshock, exp_heatshock)
+
+diff %>%
+  gost(query = .,
+       organism = "scerevisiae",
+       correction_method = "bonferroni") %>%
+  `$`(result) %>%
+  arrange(p_value) %>%
+  dplyr::slice(1:10) %>%
+  publish_gosttable(show_columns = c("term_name"))
+
