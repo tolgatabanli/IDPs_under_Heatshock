7423499455b59b62a1a23f363083f713517d9aa1
Tolga <tolgatabanli@gmail.com>
2025-01-25 22:40:44 +0100

    my trials to investigate nuetwork induced by IDPs, does not seem fruitful

--- idp interactions/cytoscape.R
+++ idp interactions/cytoscape.R
@@ -1,17 +1,29 @@
 library(tidyverse)
 library(RCy3)
+library(igraph)
 
 
 data <- read_tsv("idp interactions/BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-4.4.241.tab3.txt")
-ppi <- data %>%
+data <- data %>%
   filter(`Organism Name Interactor A` == "Saccharomyces cerevisiae (S288c)" &
            `Organism Name Interactor B` == "Saccharomyces cerevisiae (S288c)") %>%
   select(`Systematic Name Interactor A`, `Systematic Name Interactor B`,
          `Experimental System Type`, Modification) %>%
   distinct()
 
+ppi <- data %>%
+  filter(`Experimental System Type` == "physical") %>%
+  select(starts_with("Systematic"))
+
+
 common_sig_idps <- readRDS("common_sig_idps.Rds")
+ppi_graph <- graph_from_data_frame(ppi)
+neighs <- neighbors(ppi_graph, common_sig_idps)
+induced_from_idps <- induced_subgraph(ppi_graph, common_sig_idps)
 
 cytoscapePing()
-ind_sub <- 
-
+createNetworkFromIgraph(induced_from_idps)
+setNodeColorMapping(table.column="name",
+                    table.column.values = common_sig_idps,
+                    colors = "#FF0000",
+                    mapping.type = "d")
