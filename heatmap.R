8009c39da0b8107e512da89bf7ec4ed96bdc9d92
Tolga <tolgatabanli@gmail.com>
2025-02-10 22:51:50 +0100

    simplified code, less redundant

--- heatmap.R
+++ heatmap.R
@@ -31,7 +31,7 @@ dev.off()
 png("significant plots/heatmap_ref37_idps.png",
     width = 960, height = 1080, res = 150)
 pheatmap(joined_matrix, show_rownames = F,
-         main = "Heatmap from IDPs")
+         main = "Heatmap of IDPs")
 dev.off()
 
 # Heatmap with DisProt as control (?)
@@ -43,15 +43,16 @@ joined_matrix <- deseq %>%
   column_to_rownames() %>%
   as.matrix()
 
+dev.off()
+png("significant plots/heatmap_ref37_disprot.png",
+    width = 960, height = 1080, res = 150)
 pheatmap(joined_matrix, show_rownames = F,
          main = "Heatmap from DisProt")
+dev.off()
 
 # Heatmap with Non-IDPs
-joined_matrix <- readRDS("deseq_reference37.Rds") %>%
-  map(data.frame) %>%
-  map(rownames_to_column) %>%
+joined_matrix <- deseq %>%
   map(filter, !(rowname %in% idps)) %>%
-  map(select, rowname, log2FoldChange) %>%
   imap(~ setNames(.x, gsub("log2FoldChange", .y, names(.x)))) %>%
   purrr::reduce(full_join) %>%
   drop_na() %>%
